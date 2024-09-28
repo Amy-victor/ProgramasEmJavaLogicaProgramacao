e7e6b3630636369f0727ee65652c233e297f87b5
Amy <victorhugobaiajunqueira@gmail.com>
2024-09-28 16:36:53 -0300

    Adicionado Temperatura

--- Fundamentos Java/projeto/src/Alg01_ConversaoTemperatura.java
+++ Fundamentos Java/projeto/src/Alg01_ConversaoTemperatura.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Alg01_ConversaoTemperatura {
     public static void main(String[] args) {
-        System.out.println("vitor");
+    Scanner scanner = new Scanner(System.in);    
+        int resposta;
+        double celcius, kelvin, fahrenheit;
+        System.out.println("Conversor de Temperatura. Escolha qual deseja fazer a conversão.");
+        System.out.println("1- Kelvin");
+        System.out.println("2- Fareheit");
+        resposta = scanner.nextInt();
+        switch (resposta) {
+            case 1:
+                System.out.println("Digite a temperatura em Celcius:");
+                celcius = scanner.nextDouble();
+                kelvin = celcius+273.15;
+                System.out.println("temperatura em Kelvin: "+kelvin+"K");
+                break;
+            case 2:
+                System.out.println("Digite a temperatura em Celcius:");
+                celcius = scanner.nextDouble();
+                fahrenheit = (celcius*9/5)+32;
+                System.out.println("Temperatura em Fahrenheit: "+fahrenheit+"°F");
+                break;
+            default:
+                System.out.println("Não há esta escolha");
+                break;
+        }
+        
     }
 }
